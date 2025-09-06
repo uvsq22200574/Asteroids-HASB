3663f6861e567108f4aa5390e8faa2e1f7429fe7
Crhatalyk <github.nutlike018@passmail.com>
2025-09-06 04:11:59 +0200

    bug fix + test

--- src/missile.rs
+++ src/missile.rs
@@ -121,7 +121,7 @@ impl Missile {
         }
         self.rotation %= PI * 2.0;
         self.position += vec2(self.rotation.cos(), -self.rotation.sin())
-            * (self.speed + 200.0)
+            * (self.speed)
             * delta_time as f32;
 
         if self.position.x < 0.0
